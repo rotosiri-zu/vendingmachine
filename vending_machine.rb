3c6f12c6056e0b47b9c11121e3a25ae182fc45a4
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-10-17 22:08:21 +0900

    Initial commit

---
+++ vending_machine.rb
@@ -0,0 +1,8 @@
+
+coke = {id: 1, name: 'コーラ', price: 130, stock: 5}
+coffee = {id: 2, name: 'コーヒー', price: 130, stock: 5}
+water = { id: 3, name: '水', price: 100, stock: 5}
+tea = { id: 5, name: 'お茶', price: 130, stock: 5}
+orangejuice = {id: 6, name: 'オレンジジュース', price: 130, stock:5}
+all_item_list = [coke, coffee, water, tea, orangejuice]
+puts all_item_list
